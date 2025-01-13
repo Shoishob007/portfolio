e4f8d29e9f3d59dba4ba8fb4c6fc3d22fce475ca
Shoishob007 <u1704049@student.cuet.ac.bd>
2025-01-13 11:57:04 +0600

    "final commit before deploying"

--- components/footer.jsx
+++ components/footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
+        React.js & Next.js, JavaScript/TypeScript, Tailwind CSS,
         Framer Motion, React Email & Resend, Vercel hosting.
       </p>
     </footer>
